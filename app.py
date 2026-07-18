ac098368dde711e001963c9da2882deb8c280e0c
Choudhary <gaurav.choudhary@capgemini.com>
2026-07-18 10:04:45 +0530

    New added code by gaurav including python file

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Tech Farm Backend Running"
+
+@app.route("/signup", methods=["POST"])
+def signup():
+ 
+    name = request.form["name"]
+    mobile = request.form["mobile"]
+    email = request.form["email"]
+    company = request.form["company"]
+    password = request.form["password"]
+
+    try:
+
+        conn = psycopg2.connect(
+            database="Techfarm_db",
+            user="postgres",
+            password="TechFarm@123",
+            host="localhost",
+            port="5433"
+        )
+
+        cur = conn.cursor()
+
+        cur.execute("""
+            INSERT INTO users
+            (full_name,mobile_number,email,company_name,password)
+            VALUES (%s,%s,%s,%s,%s)
+        """,
+        (name,mobile,email,company,password))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+        return "Account Created Successfully ✅"
+
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(debug=True)
